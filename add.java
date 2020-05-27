8750785175d9d668c6ecda76ef0fce0be2c70fe3
root <root@sathish.us-central1-a.c.devopstraining-278417.internal>
2020-05-27 12:25:15 +0000

    this is my edited commit onthe file add.java

--- add.java
+++ add.java
@@ -2,4 +2,8 @@
       sum = num1 + num2;
 
 
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sum of these numbers: "+sum)
+      this is the new edited made by the first team.
+      i
+      
+      i am mafe the new chages in the file add .java;
